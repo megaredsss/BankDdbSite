44b702eb640003b181c182d77d3f2bc2313ee022
mastermind <maxkrasnovv@gmail.com>
2023-04-22 14:48:30 +0300

    The first

---
+++ src/main/java/com/mastermind/BankDdbSite/BankDdbSiteApplication.java
@@ -0,0 +1,13 @@
+package com.mastermind.BankDdbSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDdbSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDdbSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastermind/BankDdbSite/BankDdbSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastermind.BankDdbSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDdbSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
